7adaceefa6fcd5ecc10234e61bc1a9decac0f738
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-26 15:02:44 +0200

    post component styling

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -9,7 +9,9 @@ const Post = () => {
       <div className={styles.top}>
 
         <div className={styles.user}>
-          <Image alt='' width={40} height={40} src="https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+          <div className={styles.userImgContainer}>
+            <Image alt='' fill={true} src="https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+          </div>
           <div className={styles.userDetails}>
             <span>Marek Rykaczewski</span>
             <span>12 minutes ago</span>
@@ -24,7 +26,7 @@ const Post = () => {
       </div>
 
       <div className={styles.imgContainer}>
-      <Image alt='' fill={true} src="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+      <Image style={{objectFit: "cover"}} alt='' fill={true} src="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
       </div>
 
     </div>
