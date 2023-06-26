4200c2c76fa8f977b250796e4a6e63fc04dc81ca
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-26 14:54:25 +0200

    submit post styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <form action="" className={styles.form}>
-        <input className={styles.input} placeholder="What's on your mind?" type="text" name="" id="" />
+        <div className={styles.top}>
+          <div className={styles.imgContainer}>
+            <Image alt='' fill={true} src="https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+          </div>
+          <input className={styles.input} placeholder="What's on your mind?" type="text" name="" id="" />
+        </div>
         <button className={styles.button}>Post</button>
       </form>
       <Post />
