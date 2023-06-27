d8111f785b5f44845438df0e74f9377bf9935e2b
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-27 21:16:08 +0200

    add profile to Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,35 +3,24 @@
 import React from 'react'
 import styles from './navbar.module.css'
 import Link from 'next/link'
-
-const links = [
-    {
-        id: 1,
-        title: "Test 1",
-        url: "/"
-    },
-    {
-        id: 2,
-        title: "Test 2",
-        url: "/"
-    },
-    {
-        id: 3,
-        title: "Test 3",
-        url: "/"
-    },
-]
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
         <Link className={styles.logo} href="/">OdinBook</Link>
         <div className={styles.links}>
-        {links.map(link => (
-            <Link key={link.id} href={link.url}>
-                {link.title}
-            </Link>
-        ))}
+        <span>Marek Rykaczewski</span>
+        <div className={styles.profileContainer}>
+            <div className={styles.imgContainer}>
+                <Image alt='' fill={true} src={"https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} />
+            </div>
+            <div className={styles.arrowContainer}>
+                <div className={styles.downArrow}></div>
+            </div>
+        </div>
+
+        
         </div>
     </div>
   )
